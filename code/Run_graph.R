783d53581a1a7d3677d58bcf2547c05b8a49a960
LauraSuper <botany.laura@gmail.com>
2022-09-28 21:18:20 -0700

    main - output

--- code/Run_graph.R
+++ code/Run_graph.R
@@ -7,6 +7,9 @@ boxplot(data_PREE$Heightwk9_cm ~ data_PREE$Treatment, xlab = "Treatment", ylab =
 
 ## Save output as jpeg
 jpeg('output/phytobiome_height.jpeg')
+boxplot(data_PREE$Heightwk9_cm ~ data_PREE$Treatment, xlab = "Treatment", ylab = "Height (cm)") # plot plant height versus treatments
+dev.off()
+
 jpeg('analysis/phytobiome_height.jpeg')
 boxplot(data_PREE$Heightwk9_cm ~ data_PREE$Treatment, xlab = "Treatment", ylab = "Height (cm)") # plot plant height versus treatments
 dev.off()
