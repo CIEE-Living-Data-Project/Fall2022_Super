078383e9421366dd94c62af7cbe4377ecf3ae79d
LauraSuper <botany.laura@gmail.com>
2022-09-28 21:02:06 -0700

    main - commit!

--- Main_script.R
+++ Main_script.R
@@ -15,6 +15,15 @@
 # but the above commented code shows reproducible way to 
 # download it from OSF if had made it public:
 
+## Libraries
+# install.packages("tidyverse", "markdown","rmarkdown")
+# Did not use groundhog given already use R and it has an  issue previously noted but previous scripts mention its use
+# tinytex::install_tinytex()
+library(tinytex)
+library(tidyverse)
+library(markdown)
+library(rmarkdown)
+
 ## Read file into R
 data_PREE <- read.csv("data/CIEE_Course_Teaching_Nema_Phytobiome.csv") # Teaching phytobiome data
 head(data_PREE) # head of data
@@ -27,6 +36,16 @@ jpeg('output/phytobiome_height.jpeg')
 jpeg('analysis/phytobiome_height.jpeg')
 boxplot(data_PREE$Heightwk9_cm ~ data_PREE$Treatment, xlab = "Treatment", ylab = "Height (cm)") # plot plant height versus treatments
 dev.off()
+
+## Make a pre-registration see code folder
+# Use code Preregistration.Rmd to run:
+rmarkdown::render("code/Preregistration.Rmd")
+
+
+## Make a manuscript using .csl and .bib files in output folder
+# Move to output folder now for Manuscript_CIEE_Rmd script
+
+
 
 
 
