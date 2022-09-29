122a12fe64751e3864eb0757406d800605a01a1c
LauraSuper <botany.laura@gmail.com>
2022-09-28 21:13:55 -0700

    main - single file then  making  things in folder

---
+++ code/Run_graph.R
@@ -0,0 +1,12 @@
+## Read file into R
+data_PREE <- read.csv("data/CIEE_Course_Teaching_Nema_Phytobiome.csv") # Teaching phytobiome data
+head(data_PREE) # head of data
+
+## Make graph
+boxplot(data_PREE$Heightwk9_cm ~ data_PREE$Treatment, xlab = "Treatment", ylab = "Height (cm)") # plot plant height versus treatments
+
+## Save output as jpeg
+jpeg('output/phytobiome_height.jpeg')
+jpeg('analysis/phytobiome_height.jpeg')
+boxplot(data_PREE$Heightwk9_cm ~ data_PREE$Treatment, xlab = "Treatment", ylab = "Height (cm)") # plot plant height versus treatments
+dev.off()
