3c7e4f5bd82a705ee65c3d39bfbf7676332ae8ed
LauraSuper <botany.laura@gmail.com>
2022-09-28 19:03:55 -0700

    main - graph and main script

--- Main_script.R
+++ Main_script.R
@@ -16,8 +16,16 @@
 # download it from OSF if had made it public:
 
 ## Read file into R
+data_PREE <- read.csv("data/CIEE_Course_Teaching_Nema_Phytobiome.csv") # Teaching phytobiome data
+head(data_PREE) # head of data
 
-data_PREE <- read.csv("data/CIEE_Course_Teaching_Nema_Phytobiome.csv")
+## Make graph
+boxplot(data_PREE$Heightwk9_cm ~ data_PREE$Treatment, xlab = "Treatment", ylab = "Height (cm)") # plot plant height versus treatments
+
+## Save output as jpeg
+jpeg('output/phytobiome_height.jpeg')
+boxplot(data_PREE$Heightwk9_cm ~ data_PREE$Treatment, xlab = "Treatment", ylab = "Height (cm)") # plot plant height versus treatments
+dev.off()
 
 
 
